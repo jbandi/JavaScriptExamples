87d0f6d6eef465b8b84dfe635ce98fe3f5a04811
jbandi <dev@jonasbandi.net>
2013-12-11 23:00:49 +0100

    Adding shorties

---
+++ BadParts/shorties.js
@@ -0,0 +1,20 @@
+0 == 0
+0 == '0'
+0 == ''
+
+false == false
+false = 'false'
+false == '0'
+
+var myArray = [0]; 
+myArray == myArray; 
+myArray == !myArray
+
+var myArray = [null, undefined, []]; 
+myArray == ',,'
+
+NaN === NaN
+NaN !== NaN
+typeof NaN
+
+Math.min() < Math.max()
